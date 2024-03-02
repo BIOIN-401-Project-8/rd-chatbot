a49a2c418257b1656f00ab9f1d678873f6cc8880
Steven Tang <18170455+steventango@users.noreply.github.com>
2024-03-02 18:55:09 +0000

    Add graph query

--- src/query.py
+++ src/query.py
@@ -13,25 +13,44 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
 service_context = get_service_context()
-storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
-index = load_index_from_storage(
+storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR / "vector_store_index")
+
+#%%
+vector_store_index = load_index_from_storage(
     storage_context,
     service_context=service_context,
     show_progress=True,
 )
 query_engine = CitationQueryEngine.from_args(
-    index,
-    similarity_top_k=10,
+    vector_store_index,
+    similarity_top_k=20,
     citation_chunk_size=512,
 )
 
 # %%
-response = query_engine.query("What genes are associated with Duchenne muscular dystrophy?")
+response = query_engine.query("""What genes cause Duchenne Muscular Dystrophy?""")
 print(response)
 print("-- INTERNAL --")
 print("Source nodes:")
 for source_node in response.source_nodes:
     print(source_node.node.get_text())
     print(source_node.node.metadata)
+
+# %%
+from pathlib import Path
+storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR / "graph_store_index")
+
+knowledge_graph_index = load_index_from_storage(
+    storage_context,
+    service_context=service_context,
+    show_progress=True,
+)
+from pyvis.network import Network
+
+g = knowledge_graph_index.get_networkx_graph()
+print(g)
+net = Network(notebook=True, cdn_resources="in_line", directed=True)
+net.from_nx(g)
+net.show("example_llm.html")
 
 # %%
