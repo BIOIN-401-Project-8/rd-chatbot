477a2a900b313993948f8df73338a6a4fb31ea8e
Steven Tang <18170455+steventango@users.noreply.github.com>
2024-03-03 17:48:24 +0000

    Move citation code out of app.py

--- src/app.py
+++ src/app.py
@@ -1,18 +1,15 @@
 import logging
 import os
-import re
 import sys
 
 import chainlit as cl
-import networkx as nx
-import plotly.graph_objects as go
 from llama_index import StorageContext
 from llama_index.callbacks import CallbackManager
-from llama_index.core.response.schema import RESPONSE_TYPE
 from llama_index.prompts import PromptTemplate
 from llama_index.prompts.base import PromptType
 from llama_index.query_engine import CitationQueryEngine
 
+from citation import add_graph, get_formatted_sources
 from graph_stores import CustomNeo4jGraphStore
 from query_engine import CustomCitationQueryEngine
 from retrievers import KG_RAG_KnowledgeGraphRAGRetriever
@@ -146,97 +143,3 @@ async def main(message: cl.Message):
     add_graph(response_message)
 
     await response_message.send()
-
-
-async def get_formatted_sources(response: RESPONSE_TYPE, content: str):
-    sources = set(map(int, re.findall(r"SOURCE (\d+)", content)))
-    source_nodes = []
-    for source_node in response.source_nodes:
-        source = int(source_node.text.split(":")[0].removeprefix("Source "))
-        if source in sources:
-            source_nodes.append(source_node)
-    references = "SOURCES:\n"
-    references += "\n".join(
-        [f"{node.score:.2f}: {node.text}" for node in sorted(source_nodes, key=lambda x: x.score, reverse=True)]
-    )
-    return references
-
-
-def add_graph(response_message: cl.Message):
-    G = nx.random_geometric_graph(200, 0.125)
-    edge_x = []
-    edge_y = []
-    for edge in G.edges():
-        x0, y0 = G.nodes[edge[0]]['pos']
-        x1, y1 = G.nodes[edge[1]]['pos']
-        edge_x.append(x0)
-        edge_x.append(x1)
-        edge_x.append(None)
-        edge_y.append(y0)
-        edge_y.append(y1)
-        edge_y.append(None)
-
-    edge_trace = go.Scatter(
-        x=edge_x, y=edge_y,
-        line=dict(width=0.5, color='#888'),
-        hoverinfo='none',
-        mode='lines')
-
-    node_x = []
-    node_y = []
-    for node in G.nodes():
-        x, y = G.nodes[node]['pos']
-        node_x.append(x)
-        node_y.append(y)
-
-    node_trace = go.Scatter(
-        x=node_x, y=node_y,
-        mode='markers',
-        hoverinfo='text',
-        marker=dict(
-            showscale=True,
-            # colorscale options
-            #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
-            #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
-            #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
-            colorscale='YlGnBu',
-            reversescale=True,
-            color=[],
-            size=10,
-            colorbar=dict(
-                thickness=15,
-                title='Node Connections',
-                xanchor='left',
-                titleside='right'
-            ),
-            line_width=2))
-
-    node_adjacencies = []
-    node_text = []
-    for node, adjacencies in enumerate(G.adjacency()):
-        node_adjacencies.append(len(adjacencies[1]))
-        node_text.append('# of connections: '+str(len(adjacencies[1])))
-
-    node_trace.marker.color = node_adjacencies
-    node_trace.text = node_text
-
-    fig = go.Figure(data=[edge_trace, node_trace],
-        layout=go.Layout(
-        title='<br>Network graph made with Python',
-        titlefont_size=16,
-        showlegend=False,
-        hovermode='closest',
-        margin=dict(b=20,l=5,r=5,t=40),
-        annotations=[ dict(
-            text="Python code: <a href='https://plotly.com/ipython-notebooks/network-graphs/'> https://plotly.com/ipython-notebooks/network-graphs/</a>",
-            showarrow=False,
-            xref="paper", yref="paper",
-            x=0.005, y=-0.002 ),
-        ],
-        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
-    )
-
-    elements = [cl.Plotly(name="chart", figure=fig, display="inline")]
-
-    response_message.elements = elements
