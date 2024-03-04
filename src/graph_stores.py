376bc90c81c447a51f2ca91448c47fe4dc4fcd42
Steven Tang <18170455+steventango@users.noreply.github.com>
2024-03-04 05:12:07 +0000

    Custom organization mapping

--- src/graph_stores.py
+++ src/graph_stores.py
@@ -100,6 +100,65 @@ class CustomNeo4jGraphStore(Neo4jGraphStore):
                 )
             )
             rel_map[record["subj"]] = flattened_rels
+
+        rel_map_organization = self.get_rel_map_organization(subjs, depth, limit)
+        for subj, rels in rel_map_organization.items():
+            if subj in rel_map:
+                rel_map[subj] += rels
+            else:
+                rel_map[subj] = rels
+        return rel_map
+
+    def get_rel_map_organization(
+        self, subjs: List[str] | None = None, depth: int = 2, limit: int = 30
+    ) -> Dict[str, List[List[str]]]:
+        rel_map: Dict[Any, List[Any]] = {}
+        if subjs is None or len(subjs) == 0:
+            return rel_map
+
+        subjs = [subj.upper() for subj in subjs]
+
+        query = f"""
+            MATCH p=(n1:`{self.node_label}`)<-[:ORGANIZATION*1..{depth}]-(n2)
+            {"WHERE apoc.coll.intersection(apoc.convert.toList(n1.N_Name), $subjs)" if subjs else ""}
+            RETURN n1._N_Name AS _N_Name, n2.Address1 AS Address1, n2.Address2 AS Address2, n2.City AS City, n2.Country AS Country, n2.Email AS Email, n2.Fax as Fax, n2.Name as Name, n2.Phone as Phone, n2.State as State, n2.TollFree as TollFree, n2.URL as URL, n2.ZipCode as ZipCode
+            LIMIT {limit}
+        """
+        organizations = list(self.query(query, {"subjs": subjs}))
+
+        if not organizations:
+            return rel_map
+
+        for organization in organizations:
+            organization_description = [organization["Name"]]
+            if organization["Address1"] or organization["Address2"]:
+                organization_description.append(f"Address: ")
+                organization_description.append(f"{organization['Address1']}" if organization["Address1"] else "")
+                organization_description.append(f"{organization['Address2']}" if organization["Address2"] else "")
+            if organization["City"]:
+                organization_description.append(f"City: {organization['City']}")
+            if organization["Country"]:
+                organization_description.append(f"Country: {organization['Country']}")
+            if organization["Email"]:
+                organization_description.append(f"Email: {organization['Email']}")
+            if organization["Fax"]:
+                organization_description.append(f"Fax: {organization['Fax']}")
+            if organization["Phone"]:
+                organization_description.append(f"Phone: {organization['Phone']}")
+            if organization["State"]:
+                organization_description.append(f"State: {organization['State']}")
+            if organization["TollFree"]:
+                organization_description.append(f"TollFree: {organization['TollFree']}")
+            if organization["URL"]:
+                organization_description.append(f"URL: {organization['URL']}")
+            if organization["ZipCode"]:
+                organization_description.append(f"ZipCode: {organization['ZipCode']}")
+            for obj in organization["_N_Name"].split("|"):
+                if obj not in rel_map:
+                    rel_map[obj] = []
+                rel_map[obj].append(
+                    ("has organization", "\n".join(organization_description),)
+                )
         return rel_map
 
     def refresh_schema(self) -> None:
