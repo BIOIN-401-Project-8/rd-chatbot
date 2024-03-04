b861d5b8b476ac2f6d57f325c4302f925dd48eb3
Steven Tang <18170455+steventango@users.noreply.github.com>
2024-03-04 05:09:25 +0000

    Use intfloat/e5-large-v2 for embedding

---
+++ src/embeddings.py
@@ -0,0 +1,42 @@
+from typing import Any, List
+
+from llama_index.bridge.pydantic import PrivateAttr
+from llama_index.embeddings import BaseEmbedding
+from sentence_transformers import SentenceTransformer
+
+
+class SentenceTransformerEmbeddings(BaseEmbedding):
+    _model: SentenceTransformer = PrivateAttr()
+    _embed_batch_size: int = PrivateAttr()
+
+    def __init__(
+        self,
+        model_name_or_path: str = 'intfloat/e5-large-v2',
+        embed_batch_size: int = 1,
+        **kwargs: Any,
+    ) -> None:
+        self._model = SentenceTransformer(model_name_or_path)
+        self._embed_batch_size = embed_batch_size
+        super().__init__(**kwargs)
+
+    @classmethod
+    def class_name(cls) -> str:
+        return "SentenceTransformerEmbeddings"
+
+    async def _aget_query_embedding(self, query: str) -> List[float]:
+        return self._get_query_embedding(query)
+
+    async def _aget_text_embedding(self, text: str) -> List[float]:
+        return self._get_text_embedding(text)
+
+    def _get_query_embedding(self, query: str) -> List[float]:
+        embeddings = self._get_text_embeddings([query])
+        return embeddings[0]
+
+    def _get_text_embedding(self, text: str) -> List[float]:
+        embeddings = self._get_text_embeddings([text])
+        return embeddings[0]
+
+    def _get_text_embeddings(self, texts: List[str]) -> List[List[float]]:
+        embeddings = self._model.encode(texts, normalize_embeddings=True, batch_size=self._embed_batch_size).tolist()
+        return embeddings
--- src/service_context.py
+++ src/service_context.py
@@ -3,24 +3,20 @@ from llama_index.callbacks import CallbackManager
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms import Ollama
 
-LLM_MODEL = "starling-lm" # "mixtral"
-EMBED_MODEL = "dmis-lab/biobert-v1.1"
+from embeddings import SentenceTransformerEmbeddings
 
 
 def get_service_context(callback_manager: CallbackManager = None):
-    embed_model = HuggingFaceEmbedding(
-        model_name=EMBED_MODEL,
-        embed_batch_size=32, #8
+    embed_model = SentenceTransformerEmbeddings(
+        model_name="intfloat/e5-large-v2",
+        embed_batch_size=1,
     )
 
     llm = Ollama(
-        model=LLM_MODEL,
+        model="starling-lm",
         base_url="http://ollama:11434",
         request_timeout=30.0,
         temperature=0.0,
-        # additional_kwargs={
-        #     "num_gpu": 16,
-        # }
     )
     service_context = ServiceContext.from_defaults(embed_model=embed_model, llm=llm, callback_manager=callback_manager)
     return service_context
