55bebdd9afd4e24e730b286d24d1b3f6e4a6e10b
Steven Tang <18170455+steventango@users.noreply.github.com>
2024-03-08 07:26:50 +0000

    Add an ollama call to pull model

--- src/service_context.py
+++ src/service_context.py
@@ -1,8 +1,11 @@
+import httpx
 from llama_index import ServiceContext
 from llama_index.callbacks import CallbackManager
 from llama_index.llms import Ollama
 
 from embeddings import SentenceTransformerEmbeddings
+
+OLLAMA_MODEL = "starling-lm"
 
 
 def get_service_context(callback_manager: CallbackManager = None):
@@ -11,11 +14,13 @@ def get_service_context(callback_manager: CallbackManager = None):
         embed_batch_size=1,
     )
 
+    httpx.post("http://ollama:11434/api/pull", json={"name": OLLAMA_MODEL}, timeout=600.0)
     llm = Ollama(
-        model="starling-lm",
+        model=OLLAMA_MODEL,
         base_url="http://ollama:11434",
         request_timeout=30.0,
         temperature=0.0,
     )
+
     service_context = ServiceContext.from_defaults(embed_model=embed_model, llm=llm, callback_manager=callback_manager)
     return service_context
