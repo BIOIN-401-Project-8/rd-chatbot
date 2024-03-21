a1994bcd502c22ceb8aed99689c4545ef94db87d
Steven Tang <18170455+steventango@users.noreply.github.com>
2024-03-21 22:07:54 +0000

    Refactor metrics script

--- src/metrics_llm_only.py
+++ src/metrics_llm_only.py
@@ -1,19 +1,14 @@
 
-import pandas as pd
-import seaborn as sns
 import json
 import logging
+from typing import List, Tuple
 
 import matplotlib.pyplot as plt
-logger = logging.getLogger(__name__)
-# add file handler
-fh = logging.FileHandler("metrics.log")
-fh.setLevel(logging.DEBUG)
-logger.addHandler(fh)
+import pandas as pd
+import seaborn as sns
 
-df = pd.read_csv(
-    "/workspaces/rgd-chatbot/eval/results/KG_RAG/test_questions_one_hop_true_false_v2.csv"
-)
+logger = logging.getLogger(__name__)
+
 
 def parse_response(response: str):
     if not isinstance(response, str):
@@ -42,63 +37,64 @@ def parse_response(response: str):
             return None
 
 
-for column in df.columns:
-    if "response_" in column:
-        label_column_name = column.replace("response_", "label_")
-        df[label_column_name] = df[column].apply(parse_response)
-        correct_column_name = column.replace("response_", "correct_")
-        df[correct_column_name] = df[label_column_name] == df["label"]
+def plot(df: pd.DataFrame, columns: List[str], model_names: List[str], ylabel: str, ylim: Tuple[int, int] | None = None):
+    fig, ax = plt.subplots(figsize=(12, 6))
 
-df.to_csv("/workspaces/rgd-chatbot/eval/results/KG_RAG/test_questions_one_hop_true_false_v2_metrics.csv")
+    g = sns.barplot(
+        data=df[columns],
+        errorbar=("ci", 95),
+        capsize=.15,
+        ax=ax,
+    )
+    n = len(df)
 
-correct_columns = [column for column in df.columns if "correct_" in column]
-model_names = [column.replace("correct_", "") for column in correct_columns]
-fig, ax = plt.subplots(figsize=(12, 6))
+    g.set_title(f"{ylabel} on (n = {n}) True/False Questions")
+    g.set_ylabel("{ylabel}")
+    g.set_xlabel("Model")
+    g.set_xticks(range(len(model_names)))
+    g.set_xticklabels(model_names, rotation=45, horizontalalignment='right')
+    g.set(ylim=ylim)
+    g.yaxis.set_major_locator(plt.MaxNLocator(11))
+    g.yaxis.grid(True)
 
-g = sns.barplot(
-    data=df[correct_columns],
-    errorbar=("ci", 95),
-    capsize=.15,
-    ax=ax,
-)
-n = len(df)
+    plt.savefig(
+        f"/workspaces/rgd-chatbot/eval/results/KG_RAG/one_hop_true_false_v2_df_{ylabel.lower()}.v2.png",
+        bbox_inches="tight",
+    )
 
 
-g.set_title(f"Accuracy on (n = {n}) True/False Questions")
-g.set_ylabel("Accuracy")
-g.set_xlabel("Model")
-g.set_xticks(range(len(model_names)))
-g.set_xticklabels(model_names, rotation=45, horizontalalignment='right')
-g.set(ylim=(0, 1))
-g.yaxis.set_major_locator(plt.MaxNLocator(11))
-g.yaxis.grid(True)
 
-plt.savefig(
-    "/workspaces/rgd-chatbot/eval/results/KG_RAG/one_hop_true_false_v2_df.v2.png",
-    bbox_inches="tight",
-)
+def main():
+    fh = logging.FileHandler("metrics.log")
+    fh.setLevel(logging.DEBUG)
+    logger.addHandler(fh)
 
-time_columns = [column for column in df.columns if "time_" in column]
-for time_column in time_columns:
-    df[time_column + "_seconds"] = df[time_column].apply(lambda x: pd.to_timedelta(x).total_seconds())
+    input_csv = "/workspaces/rgd-chatbot/eval/results/KG_RAG/test_questions_one_hop_true_false_v2.csv"
+    output_csv = input_csv.replace(".csv", "_metrics.csv")
 
-fig, ax = plt.subplots(figsize=(12, 6))
-g = sns.barplot(
-    data=df[[column + "_seconds" for column in time_columns]],
-    errorbar=("ci", 95),
-    capsize=.15,
-    ax=ax,
-)
-g.set_title(f"Time to predict on (n = {n}) True/False Questions")
-g.set_ylabel("Time (s)")
-g.set_xlabel("Model")
-g.set_xticks(range(len(model_names)))
-g.set_xticklabels(model_names, rotation=45, horizontalalignment='right')
-g.yaxis.set_major_locator(plt.MaxNLocator(11))
-g.yaxis.grid(True)
+    df = pd.read_csv(input_csv)
+    for column in df.columns:
+        if "response_" in column:
+            label_column_name = column.replace("response_", "label_")
+            df[label_column_name] = df[column].apply(parse_response)
+            correct_column_name = column.replace("response_", "correct_")
+            df[correct_column_name] = df[label_column_name] == df["label"]
 
-plt.savefig(
-    "/workspaces/rgd-chatbot/eval/results/KG_RAG/one_hop_true_false_v2_df_time.v2.png",
-    bbox_inches="tight",
-)
+    df.to_csv(output_csv)
 
+    correct_columns = [column for column in df.columns if "correct_" in column]
+    model_names = [column.replace("correct_", "") for column in correct_columns]
+
+    plot(df, correct_columns, model_names, "Accuracy", (0, 1))
+
+    time_columns = [column for column in df.columns if "time_" in column]
+    for time_column in time_columns:
+        df[time_column + "_seconds"] = df[time_column].apply(lambda x: pd.to_timedelta(x).total_seconds())
+
+    time_seconds_columns = [column + "_seconds" for column in time_columns]
+
+    plot(df, time_seconds_columns, model_names, "Time")
+
+
+if __name__ == "__main__":
+    main()
