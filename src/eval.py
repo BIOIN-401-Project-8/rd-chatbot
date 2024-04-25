14f755b3da58bd8b293bff700c3a4f35fd5deb54
Steven Tang <18170455+steventango@users.noreply.github.com>
2024-04-25 20:47:24 +0000

    Fix paths

--- src/eval.py
+++ src/eval.py
@@ -61,9 +61,9 @@ def eval_llm():
         # "openai:gpt-4-turbo",
     ]
 
-    output_file = f"/workspaces/rgd-chatbot/eval/results/RD/test_questions.csv"
+    output_file = f"/workspaces/rd-chatbot/eval/results/RD/test_questions.csv"
 
-    df = pd.read_csv("/workspaces/rgd-chatbot/eval/data/RD/test_questions.csv")
+    df = pd.read_csv("/workspaces/rd-chatbot/eval/data/RD/test_questions.csv")
 
     if Path(output_file).exists():
         df = pd.read_csv(output_file)
@@ -100,34 +100,47 @@ def eval_llm():
     models_rag = [
         "llama3:8b-instruct-q4_0",
     ]
-    for model_name in models_rag:
-        df_view = df
-        slug = model_name
-        slug += "_rag"
-        if f"response_{slugify(model_name)}" in df.columns:
-            df_view = df[df[f"error_{slugify(model_name)}"] == True]
-        if len(df_view) == 0:
-            continue
-        llm = get_llm(llm_model_name=model_name)
+    # for model_name in models_rag:
+    #     df_view = df
+    #     slug = model_name
+    #     slug += "_rag"
+    #     if f"response_{slugify(model_name)}" in df.columns:
+    #         df_view = df[df[f"error_{slugify(model_name)}"] == True]
+    #     if len(df_view) == 0:
+    #         continue
+    #     llm = get_llm(llm_model_name=model_name)
 
-        for index, row in tqdm(df_view.iterrows(), total=len(df_view)):
-            slug = slugify(model_name)
-            error = False
-            start = time.time()
-            try:
-                question = row["question"]
-                response = run_pipeline(model_name, question)
-            except Exception as e:
-                logger.exception(f"Failed to get response for {question}")
-                response = str(e)
-                error = True
-            end = time.time()
+    #     for index, row in tqdm(df_view.iterrows(), total=len(df_view)):
+    #         slug = slugify(model_name)
+    #         error = False
+    #         start = time.time()
+    #         try:
+    #             question = row["question"]
+    #             response = run_pipeline(model_name, question)
+    #         except Exception as e:
+    #             logger.exception(f"Failed to get response for {question}")
+    #             response = str(e)
+    #             error = True
+    #         end = time.time()
 
-            df_view.loc[index, f"response_{slug}"] = response
-            df_view.loc[index, f"time_{slug}"] = timedelta(seconds=end - start)
-            df_view.loc[index, f"error_{slug}"] = error
-            df.loc[df_view.index, df_view.columns] = df_view
-            df.to_csv(output_file, index=False)
+    #         df_view.loc[index, f"response_{slug}"] = response
+    #         df_view.loc[index, f"time_{slug}"] = timedelta(seconds=end - start)
+    #         df_view.loc[index, f"error_{slug}"] = error
+    #         df.loc[df_view.index, df_view.columns] = df_view
+    #         df.to_csv(output_file, index=False)
+
+    response_columns = [column for column in df.columns if column.startswith("response_")]
+    # we want to add columns for each model
+    for column in response_columns:
+        slug = column.replace("response_", "")
+        if f"relevant_{slug}" not in df.columns:
+            df[f"relevant_{slug}"] = None
+        if f"correct_{slug}" not in df.columns:
+            df[f"correct_{slug}"] = None
+        if f"complete_{slug}" not in df.columns:
+            df[f"complete_{slug}"] = None
+        if f"correctly_cited_{slug}" not in df.columns:
+            df[f"correctly_cited_{slug}"] = None
 
     return df
 
@@ -151,7 +164,7 @@ def eval_translation(df: pd.DataFrame):
 def eval_one_translation(
     df: pd.DataFrame, method: str, translator: BaseTranslator, source: str, target: str, column: str, slug: str
 ):
-    output_file = f"/workspaces/rgd-chatbot/eval/results/RD/gard_corpus_translation.csv"
+    output_file = f"/workspaces/rd-chatbot/eval/results/RD/gard_corpus_translation.csv"
     logging.info(f"Translating {slug}")
     for index, row in tqdm(df.iterrows(), total=len(df)):
         if slug in row and pd.notna(row[slug]):
@@ -174,10 +187,10 @@ def eval_one_translation(
 
 def main():
     df = eval_llm()
-    # if Path(f"/workspaces/rgd-chatbot/eval/results/RD/gard_corpus_translation.csv").exists():
-    #     df = pd.read_csv("/workspaces/rgd-chatbot/eval/results/RD/gard_corpus_translation.csv")
+    # if Path(f"/workspaces/rd-chatbot/eval/results/RD/gard_corpus_translation.csv").exists():
+    #     df = pd.read_csv("/workspaces/rd-chatbot/eval/results/RD/gard_corpus_translation.csv")
     # else:
-    #     df = pd.read_csv("/workspaces/rgd-chatbot/eval/data/RD/gard_corpus.csv")
+    #     df = pd.read_csv("/workspaces/rd-chatbot/eval/data/RD/gard_corpus.csv")
     # df = df.head(100)
     # df = eval_translation(df)
 
